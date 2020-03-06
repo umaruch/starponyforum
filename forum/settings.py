a70b3eb35d76359385bdbb22cd487862b0185436
umaruch <umaruch@xlaomi.com>
2020-03-06 14:05:17 +0500

    ADD MODELS AND NEMNOIIIKO Settings

--- forum/settings.py
+++ forum/settings.py
@@ -125,3 +125,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
--- forummain/models.py
+++ forummain/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Theme(models.Model):
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    title = models.CharField(max_length=120)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Message(models.Model):
+    theme = models.ForeignKey(Theme,on_delete = models.CASCADE)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    text = models.TextField(max_length=160, blank=True )
+    image = models.FileField(blank=True )
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.text
